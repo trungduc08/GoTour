3a762653029faba72e01db117f809a9cf101fda6
trungduc08 <nguyentrugduc248@gmail.com>
2018-12-25 17:01:54 +0700

    make exercise-map

--- Lession2.go
+++ Lession2.go
@@ -6,6 +6,11 @@ type Vector struct {
 	X int
 	Y int
 }
+type Vertex struct {
+	Lat, Long float64
+}
+
+// var m map[string]Vertex
 
 var (
 	v1 = Vector{1, 2}  // has type Vertex
@@ -13,36 +18,29 @@ var (
 	v3 = Vector{}      // X:0 and Y:0
 	p  = &Vector{1, 2} // has type *Vertex
 )
+var lstMap = map[string]Vertex{
+	"map1": {10.10, 20.20},
+	"map2": {30.30, 40.40},
+}
 
 func main() {
-	// s := []int{2, 3, 5, 7, 11, 13}
-	// s = s[:0]
+	m := make(map[string]int)
 
-	// fmt.Println(len(s), s)
-	// fmt.Println(cap(s), s)
-	// a := make([]int, 5)
-	// printSlice("a", a)
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
 
-	// b := make([]int, 0, 5)
-	// printSlice("b", b)
+	m["Answer1"] = 48
+	fmt.Println("The value:", m["Answer1"])
 
-	// c := b[:2]
-	// printSlice("c", c)
+	m["Answer2"] = 49
+	fmt.Println("The value:", m["Answer2"])
 
-	// d := c[2:5]
-	// printSlice("d", d)
-	pow := make([]int, 10)
-	for i := range pow {
-		pow[i] = 1 << uint(i) // == 2**i
-		// fmt.Printf("%d\n", pow[i])
-	}
-	fmt.Println("----------------------------")
-	for _, value := range pow {
-		fmt.Printf("%d\n", value)
-	}
+	m["Answer6"] = 1
+	fmt.Println("The value:", m["Answer6"])
 
-}
-func printSlice(s string, x []int) {
-	fmt.Printf("%s len=%d cap=%d %v\n",
-		s, len(x), cap(x), x)
+	// delete(m, "Answer")
+	// fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer6"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
